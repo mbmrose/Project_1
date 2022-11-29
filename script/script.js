6937d438a99fd9a0379aeaeda5312aa4377b6616
Maia Bell <maiarose04@gmail.com>
2022-11-28 22:16:39 -0800

    realized I messed up my css and started over now working on javascript (Will attempt to correct errors later)

---
+++ script/script.js
@@ -0,0 +1,15 @@
+
+
+const darkMode = document.querySelector(".button2");
+
+darkMode.addEventListener("click", function changeText(){
+    const initialText = "Dark Theme"
+    if (darkMode.textContent === "Dark Theme")
+    {darkMode.textContent = "Light Theme"}
+    else{
+        darkMode.textContent = initialText;
+    }
+})
+
+document.body.classList.toggle("darkmode");
+
