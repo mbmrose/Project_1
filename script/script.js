c28ae33b5b2f69b53b46594c5e467b4e19646d5e
Maia Bell <maiarose04@gmail.com>
2022-11-29 21:54:41 -0800

    Makes console log when button clicked

--- script/script.js
+++ script/script.js
@@ -1,4 +1,31 @@
+document.body.addEventListener("click", Clicking)
 
+function Clicking(event){
+    console.log("clicked", event.target.textContent);
+
+    if(event.target.textContent === "Dark Theme"){
+        console.log("Dark Theme Clicked");
+        changebg();
+    }
+    else if(event.target.textContent === "New Note"){
+        console.log("New Note Clicked");
+    }
+    else if(event.target.textContent === "Save"){
+        console.log("Save Button Clicked");
+    }
+    else if(event.target.textContent === "Cancel"){
+        console.log("Cancel Button Clicked")
+    }
+}
+
+
+// Makes Darkmode 
+
+function changebg(){
+    document.body.classList.toggle("bgcolor")
+}
+
+// Makes Darkmode button text switch to "Light Mode" on click
 
 const darkMode = document.querySelector(".button2");
 
@@ -12,5 +39,10 @@ darkMode.addEventListener("click", function changeText(){
 })
 
 
-document.body.classList.toggle("darkmode");
+
+
+
+
+
+
 
