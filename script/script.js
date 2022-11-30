b6f0d74320c4f378a9d3ec2d065fda155c900c78
Maia Bell <maiarose04@gmail.com>
2022-11-30 12:40:46 -0800

    Created a prompt and saved items to an array which is then appended to the ul

--- script/script.js
+++ script/script.js
@@ -1,4 +1,13 @@
+// Assignment 2
+
+
+// Event listener
+
 document.body.addEventListener("click", Clicking)
+
+
+// All of the consts
+
 const asidebg = document.querySelector("aside");
 const newbutton = document.querySelector(".button1");
 const darkMode = document.querySelector(".button2");
@@ -8,6 +17,9 @@ const internet = document.querySelector(".internet");
 const cancelbutton = document.querySelector(".button4");
 const notebox = document.querySelector(".notebox");
 const textColor = document.querySelector(".text");
+
+
+// Function for clicking buttons
 
 function Clicking(event){
     console.log("clicked", event.target.textContent);
@@ -27,15 +39,16 @@ function Clicking(event){
     }
     else if(event.target.textContent === "Save"){
         console.log("Save Button Clicked");
+        notelist();
     }
     else if(event.target.textContent === "Cancel"){
         cancelbtn();
-        console.log("Cancel Button Clicked")
+        console.log("Cancel Button Clicked");
     }
 }
 
 
-// Makes Darkmode 
+// Makes Darkmode
 
 function changebg(){
     document.body.classList.toggle("bgcolor");
@@ -50,6 +63,7 @@ function changebg(){
     textColor.classList.toggle("textcolor");
 }
 
+
 // Makes Darkmode button text switch to "Light Mode" on click
 
 darkMode.addEventListener("click", function changeText(){
@@ -61,11 +75,17 @@ darkMode.addEventListener("click", function changeText(){
     }
 })
 
+
+// Makes cancel button remove save and notebox
+
 function cancelbtn(){
     cancelbutton.classList.add("hideitem");
     savebutton.classList.add("hideitem");
     notebox.classList.add("hideitem");
 }
+
+
+// Makes new button add save and notebox
 
 function newbtn(){
     cancelbutton.classList.remove("hideitem");
@@ -73,6 +93,40 @@ function newbtn(){
     notebox.classList.remove("hideitem");
 }
 
+
+// Clears text when clicking "New Note"
+
 function textclearing(){
     notebox.value = ""
+}
+
+
+
+
+let notesArray = [{title : "Note One", body: "This is my first note"}]
+
+function notelist(){
+    const noteTitle = prompt("Enter a note title...");
+    const noteInput = document.querySelector(".notebox");
+
+    let notes = {
+        title: noteTitle,
+        body: noteInput.value,
+    }
+
+    noteTitle.value = ""
+    noteInput.value = ""
+
+    addToList(notes);
+
+    notesArray.push(notes);
+
+    console.log(notesArray)
+}
+
+function addToList(notes){
+    const myNotes = document.querySelector(".notes");
+    const newNote = document.createElement("li");
+        newNote.textContent = `${notes.title}`;
+        myNotes.appendChild(newNote)
 }
