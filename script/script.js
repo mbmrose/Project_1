23ee8b893a3c359b747e0e890f9e14267b959c45
Maia Bell <maiarose04@gmail.com>
2022-11-29 23:21:13 -0800

    turns screen to darkmode when button is clicked

--- script/script.js
+++ script/script.js
@@ -3,8 +3,12 @@ document.body.addEventListener("click", Clicking)
 function Clicking(event){
     console.log("clicked", event.target.textContent);
 
-    if(event.target.textContent === "Dark Theme"){
+    if(event.target.textContent === "Light Theme"){
         console.log("Dark Theme Clicked");
+        changebg();
+    }
+    else if (event.target.textContent === "Dark Theme"){
+        console.log("Light Theme Clicked");
         changebg();
     }
     else if(event.target.textContent === "New Note"){
@@ -20,14 +24,30 @@ function Clicking(event){
 
 
 // Makes Darkmode 
+const asidebg = document.querySelector("aside");
+const newbutton = document.querySelector(".button1");
+const darkMode = document.querySelector(".button2");
+const savebutton = document.querySelector(".button3");
+const linkedin = document.querySelector(".linkedin");
+const internet = document.querySelector(".internet");
+const cancelbutton = document.querySelector(".button4");
+const notebox = document.querySelector(".notebox");
+const textColor = document.querySelector(".text");
 
 function changebg(){
-    document.body.classList.toggle("bgcolor")
+    document.body.classList.toggle("bgcolor");
+    asidebg.classList.toggle("asidecolor");
+    newbutton.classList.toggle("newbuttoncolor");
+    darkMode.classList.toggle("darkbutton");
+    savebutton.classList.toggle("savecolor");
+    linkedin.classList.toggle("lncolor");
+    internet.classList.toggle("internetcolor");
+    cancelbutton.classList.toggle("cancelcolor")
+    notebox.classList.toggle("noteboxcolor");
+    textColor.classList.toggle("textcolor");
 }
 
 // Makes Darkmode button text switch to "Light Mode" on click
-
-const darkMode = document.querySelector(".button2");
 
 darkMode.addEventListener("click", function changeText(){
     const initialText = "Dark Theme"
